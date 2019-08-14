ff72017ed66a56bedd43a40cfce1eaf2e83bedc4
manel <emanuel.cortes@pirelli.com>
2019-08-14 19:00:00 -0300

    First commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+// MVC  - Model, View, Controller
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-goxs6.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+
+
+server.listen(3333);
